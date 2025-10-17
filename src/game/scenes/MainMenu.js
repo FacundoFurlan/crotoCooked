6453ef58fe10683daf041d7e140f676cdcb5709d
ErwinAndino <erwin.andino@hotmail.com>
2025-10-17 18:50:57 -0300

    se añadio un menu que se puede usar con mando y teclado, se arreglo que el texto ORDEN no se eliminaba al entregar una orden

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -1,29 +1,77 @@
+import InputSystem, { INPUT_ACTIONS } from "../../utils/InputSystem";
 export class MainMenu extends Phaser.Scene {
     constructor() {
         super("MainMenu");
     }
 
     create() {
+
+        this.inputSystem = new InputSystem(this.input);
+        this.inputSystem.configureKeyboard({
+            [INPUT_ACTIONS.UP]: [Phaser.Input.Keyboard.KeyCodes.W],
+            [INPUT_ACTIONS.DOWN]: [Phaser.Input.Keyboard.KeyCodes.S],
+            [INPUT_ACTIONS.LEFT]: [Phaser.Input.Keyboard.KeyCodes.A],
+            [INPUT_ACTIONS.RIGHT]: [Phaser.Input.Keyboard.KeyCodes.D],
+            [INPUT_ACTIONS.SOUTH]: [Phaser.Input.Keyboard.KeyCodes.X],
+            [INPUT_ACTIONS.EAST]: [Phaser.Input.Keyboard.KeyCodes.C],
+            [INPUT_ACTIONS.WEST]: [Phaser.Input.Keyboard.KeyCodes.Z]
+        }, "player1");
+        this.inputSystem.configureKeyboard({
+            [INPUT_ACTIONS.UP]: [Phaser.Input.Keyboard.KeyCodes.UP],
+            [INPUT_ACTIONS.DOWN]: [Phaser.Input.Keyboard.KeyCodes.DOWN],
+            [INPUT_ACTIONS.LEFT]: [Phaser.Input.Keyboard.KeyCodes.LEFT],
+            [INPUT_ACTIONS.RIGHT]: [Phaser.Input.Keyboard.KeyCodes.RIGHT],
+            [INPUT_ACTIONS.SOUTH]: [Phaser.Input.Keyboard.KeyCodes.K],
+            [INPUT_ACTIONS.EAST]: [Phaser.Input.Keyboard.KeyCodes.L],
+            [INPUT_ACTIONS.WEST]: [Phaser.Input.Keyboard.KeyCodes.J]
+        }, "player2");
+
         const { width, height } = this.scale;
 
-        this.add.text(width/2, height/2, "Press A for coop, B for vs", { fontSize: "32px", color: "#fff", fontFamily: "MyFont" }).setOrigin(0.5);
-
         this.registry.set("actualLevel", 1);
-        
-        this.coopKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        this.vsKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.B);
+
+        this.titleText = this.add.text(width / 2, height / 4, "POLLOS BIFES Y ACHICORIA: definitive edition", { fontSize: "32px", color: "#fff", fontFamily: "MyFont" }).setOrigin(0.5);
+        this.coopText = this.add.text(width / 2, height / 2, "Coop", { fontSize: "32px", color: "#fff", fontFamily: "MyFont" }).setOrigin(0.5);
+        this.versusText = this.add.text(width / 2, height / 1.5, "Versus", { fontSize: "32px", color: "#fff", fontFamily: "MyFont" }).setOrigin(0.5);
+
+        this.selector = 1
+        this.highlightText()
     }
-    
-    update(){
-        if(Phaser.Input.Keyboard.JustDown(this.coopKey)){
-            this.registry.set("mode", 1);
-            this.scene.start("Game"); 
-            this.scene.launch("HUD"); // lanzar HUD encima del Game
+
+    update() {
+
+        if (this.inputSystem.isJustPressed(INPUT_ACTIONS.UP, "player1") || this.inputSystem.isJustPressed(INPUT_ACTIONS.UP, "player2")) {
+            console.log("PA RRIBA")
+            this.selector = Math.min(1, this.selector + 1)
+            this.highlightText()
         }
-        if(Phaser.Input.Keyboard.JustDown(this.vsKey)){
-            this.registry.set("mode", 2);
-            this.scene.start("Game"); 
-            this.scene.launch("HUD"); // lanzar HUD encima del Game
+        if (this.inputSystem.isJustPressed(INPUT_ACTIONS.DOWN, "player1") || this.inputSystem.isJustPressed(INPUT_ACTIONS.DOWN, "player2")) {
+            console.log("PA BAJO")
+            this.selector = Math.max(0, this.selector - 1)
+            this.highlightText()
+
         }
+        if (this.inputSystem.isJustPressed(INPUT_ACTIONS.WEST, "player1") || this.inputSystem.isJustPressed(INPUT_ACTIONS.WEST, "player2")) {
+            console.log("SI SI SI SI")
+            if (this.selector === 1) { // COOP
+                this.registry.set("mode", 1);
+                this.scene.start("Game");
+                this.scene.launch("HUD"); // lanzar HUD encima del Game
+            }
+            if (this.selector === 0) { // VERSUS
+                this.registry.set("mode", 2);
+                this.scene.start("Game");
+                this.scene.launch("HUD"); // lanzar HUD encima del Game
+            }
+        }
+
+    }
+    highlightText() {
+        this.coopText.setColor("#fff");
+        this.versusText.setColor("#fff");
+        let text = null;
+        if (this.selector === 1) text = this.coopText;
+        if (this.selector === 0) text = this.versusText;
+        text.setColor("#2ed12eff");
     }
 }
