c53944ab15189d04dd8a5d1e9dcbf01558cd71a8
NeroWork <facundofurlan7@gmail.com>
2025-10-17 19:07:31 -0300

    nuevo Circular timer v1

--- src/game/classes/asador.js
+++ src/game/classes/asador.js
@@ -1,3 +1,4 @@
+import { CircularTimer } from "./CircularTimer";
 import { KitchenBox } from "./kitchenBox";
 
 export class Asador extends KitchenBox {
@@ -11,6 +12,9 @@ export class Asador extends KitchenBox {
         this.textureKey = textureKey;
         this.holdingItem = false;
         this.itemHolded = null;
+        this.numeroDeEtapas = 0;
+        this.etapas = {};
+        this.etapaActual = null;
 
         this.hasCoal = false;
         this.iconoCarbon = scene.add.image(x, y - 10, "iconoCarbon");
@@ -22,6 +26,8 @@ export class Asador extends KitchenBox {
         this.durationCoal = 10000;
         this.actionSound = this.scene.coccionAudio
         this.actionFinish = this.scene.coccionListoAudio
+
+        this.circleTimer = new CircularTimer(scene, x, y, 16, this.cookDuration, () => { this.finishCook() }, 2)
     }
 
     onInteract(player) {
@@ -51,7 +57,6 @@ export class Asador extends KitchenBox {
         } else if (player.holdingItem && !this.holdingItem) { //si el jugador tiene algo y esto no
 
             const isCompatible = this.checkIfItemCompatible(player);
-            console.log(isCompatible)
             if (isCompatible && this.coalFrame === 0) {
                 this.iconoCarbon.setVisible(true);
             }
@@ -61,6 +66,35 @@ export class Asador extends KitchenBox {
             this.checkIfItemCanGo(player);
 
         }
+    }
+
+    checkIfItemCompatible(player) {
+        // Protege contra null
+        if (!player.itemHolded) return false;
+
+        // Si el timer está activo, no es compatible
+        if (this.circleTimer.active) return false;
+
+        // Si el aparato acepta el item
+        if (this.aparatoAccepts[player.itemHolded.textureKey]) {
+            console.log("%cEntro por aca", "color: green");
+            this.scene.tweens.killTweensOf(player.itemHolded);
+            console.log("se intento poner algo: ", player.itemHolded.dataIngredient);
+
+            this.itemHolded = player.itemHolded;
+            this.holdingItem = true;
+            player.holdingSM.changeState("none", { player: player });
+            this.itemHolded.setPosition(this.body.center.x, this.body.center.y);
+            this.itemHolded.setVisible(true);
+
+            // si es una mesa y no puede cortar entonces no se cocina
+            if (this.textureKey === "mesa" && !this.cortar) return false;
+            this.startCook();
+
+            return true; // compatible
+        }
+
+        return false; // no compatible
     }
 
     checkIfItemCanGo(player) {
@@ -73,19 +107,52 @@ export class Asador extends KitchenBox {
         player.holdingSM.changeState("ingredient", { player: player, ingredient: this.itemHolded });
         this.itemHolded = null;
         this.holdingItem = false;
+        this.numeroDeEtapas = 0;
+        this.etapas = {};
+        this.etapaActual = null;
     }
 
     startCook() {
         if (this.itemHolded && this.itemHolded.dataIngredient.next && this.itemHolded.dataIngredient.next[this.textureKey]) {
             if (this.hasCoal) {
+                let numeroDeEtapas = 0;
+
+                let data = this.itemHolded.dataIngredient
+                console.log("data    ", data)
+
+                while( data.next && data.next[this.textureKey]){
+                    numeroDeEtapas++;
+                    
+                    let nextTexture = data.next[this.textureKey];
+                    this.etapas[numeroDeEtapas] = nextTexture;
+                    console.log("next texture:    ", nextTexture)
+                    
+                    data = this.scene.ingredientesAtlas[nextTexture];
+                }
+
+                this.numeroDeEtapas = numeroDeEtapas;
+                console.log("Numero de etapas:    ", numeroDeEtapas)
+
                 console.log("%cSe inicia reloj", "color: aqua")
-                this.circleTimer.start()
+                this.circleTimer.start((numeroDeEtapas) * 3000)
                 if (this.actionSound) {
                     this.actionSound.play();
                 }
                 this.textureCoal.setVisible(true);
             }
         }
+    }
+
+    finishCook() {
+        this.itemHolded.cook(this.textureKey);
+        this.actionFinish.play()
+        console.log("NEW COOKED ITEM: ", this.itemHolded.dataIngredient)
+        if (this.actionSound) {
+            this.actionSound.stop();
+        }
+        this.numeroDeEtapas = 0;
+        this.etapas = {};
+        this.etapaActual = null;
     }
 
 
@@ -111,6 +178,14 @@ export class Asador extends KitchenBox {
                         if (this.actionSound) this.actionSound.stop();
                     }
                     break;
+                }
+            }
+
+            if(this.circleTimer.active){
+                const progress = this.circleTimer.progress;
+                if(Math.floor(progress/3000) > this.etapaActual){
+                    this.etapaActual = Math.floor(progress/3000);
+                    this.itemHolded.cook(this.textureKey)
                 }
             }
         }
