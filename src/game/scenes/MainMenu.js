6046cc8ecde974ca7f7d84f4cc00f057017c2b7b
Erwin Andino <erwin.andino@hotmail.com>
2025-10-25 21:43:08 -0300

    cambiados los textos del menu para que queden mejor y se adapten al tamano de pantalla, se agrego un boton scoreboard para despues agregar esa funcionalidad

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -54,36 +54,95 @@ export class MainMenu extends Phaser.Scene {
         this.registry.set("actualLevel", 1);
 
         this.add.image(320, 180, "menuBG")
-        this.westText = this.add.text(width / 2 + 210, height / 2 + 40, "juntar", { fontSize: "24px", color: "#303decff", fontFamily: "MyFont" }).setOrigin(0.5);
+        this.westText = this.add.text(width / 1.2, height / 1.65, "juntar", {
+            fontSize: "24px",
+            color: "#303decff",
+            fontFamily: "MyFont"
+        }).setOrigin(0.5);
+
         this.westText.angle = 15
-        this.southText = this.add.text(width / 2 + 260, height / 2 + 70, "dash", { fontSize: "24px", color: "#303decff", fontFamily: "MyFont" }).setOrigin(0.5);
+
+        this.southText = this.add.text(width / 1.1, height / 1.45, "dash", {
+            fontSize: "24px",
+            color: "#303decff",
+            fontFamily: "MyFont"
+        }).setOrigin(0.5);
+
         this.southText.angle = -10
-        this.eastText = this.add.text(width / 2 + 280, height / 2 - 0, "lanzar", { fontSize: "24px", color: "#303decff", fontFamily: "MyFont" }).setOrigin(0.5);
+
+        this.eastText = this.add.text(width / 1.05, height / 2, "lanzar", {
+            fontSize: "24px",
+            color: "#303decff",
+            fontFamily: "MyFont"
+        }).setOrigin(0.5);
+
         this.eastText.angle = 10
 
-        this.titleText1 = this.add.text(width / 2 - 186, height / 4, "POLLO, BIFES", { fontSize: "30px", color: "#fff", fontFamily: "MyFont" }).setOrigin(0.5);
-        this.titleText1.angle = -5
-        this.titleText1 = this.add.text(width / 2 - 182, height / 4 + 20, import.meta.env.VITE_TITLE, { fontSize: "30px", color: "#fff", fontFamily: "MyFont" }).setOrigin(0.5);
-        this.titleText1.angle = -5
-        this.coopText = this.add.text(width / 2 - 170, height / 2.3, "Coop", { fontSize: "30px", color: "#fff", fontFamily: "MyFont" }).setOrigin(0.5);
-        this.versusText = this.add.text(width / 2 - 160, height / 1.8, "Versus", { fontSize: "30px", color: "#fff", fontFamily: "MyFont" }).setOrigin(0.5);
+        this.menuText = this.add.text(width / 10, height / 7, "MENU", {
+            fontSize: "50px",
+            color: "#000000",
+            fontFamily: "MyFont",
+            stroke: "#fff",
+            strokeThickness: 4,
+        }).setOrigin(0);
 
-        this.languageText = this.add.text(
-            width / 2 - 150,
-            height / 1.5,
-            getPhrase(this.language2),
-            {
-                fontSize: "30px", color: "#fff", fontFamily: "MyFont"
-            }
-        ).setOrigin(0.5);
+        this.menuText.angle = -5
 
+        //Titulo PByA
+        this.polloText = this.add.text(width / 9, height / 3.7, "POLLO", {
+            fontSize: "36px",
+            color: "#fff",
+            fontFamily: "MyFont"
+        }).setOrigin(0);
+        this.polloText.angle = -6
+
+        this.bifesText = this.add.text(width / 8.5, height / 3, "BIFES Y", {
+            fontSize: "36px",
+            color: "#fff",
+            fontFamily: "MyFont"
+        }).setOrigin(0);
+        this.bifesText.angle = -6
+
+        this.achicoriaText = this.add.text(width / 8, height / 2.5, import.meta.env.VITE_TITLE, {
+            fontSize: "36px",
+            color: "#fff",
+            fontFamily: "MyFont"
+        }).setOrigin(0);
+        this.achicoriaText.angle = -6
+
+        //Botones
+        this.coopText = this.add.text(width / 6, height / 2, "◦ Cooperativo", {
+            fontSize: "24px",
+            color: "#fff",
+            fontFamily: "MyFont"
+        }).setOrigin(0);
         this.coopText.angle = -5;
+
+        this.versusText = this.add.text(width / 5.8, height / 1.8, "◦ Versus", {
+            fontSize: "30px",
+            color: "#fff",
+            fontFamily: "MyFont"
+        }).setOrigin(0);
         this.versusText.angle = -5;
+
+        this.scoreboardText = this.add.text(width / 5.5, height / 1.6, "◦ Scoreboard", {
+            fontSize: "24px",
+            color: "#fff",
+            fontFamily: "MyFont"
+        }).setOrigin(0);
+        this.scoreboardText.angle = -5;
+
+        this.languageText = this.add.text(width / 5.25, height / 1.45, "◦ " + getPhrase(this.language2), {
+            fontSize: "30px",
+            color: "#fff",
+            fontFamily: "MyFont"
+        }
+        ).setOrigin(0);
         this.languageText.angle = -5;
 
         this.#updatedTextInScene = this.languageText; // o cualquier texto que quieras
 
-        this.selector = 2
+        this.selector = 3
         this.highlightText()
     }
 
@@ -99,7 +158,7 @@ export class MainMenu extends Phaser.Scene {
 
         if (this.inputSystem.isJustPressed(INPUT_ACTIONS.UP, "player1") || this.inputSystem.isJustPressed(INPUT_ACTIONS.UP, "player2")) {
             console.log("PA RRIBA")
-            this.selector = Math.min(2, this.selector + 1)
+            this.selector = Math.min(3, this.selector + 1)
             this.highlightText()
         }
         if (this.inputSystem.isJustPressed(INPUT_ACTIONS.DOWN, "player1") || this.inputSystem.isJustPressed(INPUT_ACTIONS.DOWN, "player2")) {
@@ -110,15 +169,18 @@ export class MainMenu extends Phaser.Scene {
         }
         if (this.inputSystem.isJustPressed(INPUT_ACTIONS.WEST, "player1") || this.inputSystem.isJustPressed(INPUT_ACTIONS.WEST, "player2")) {
             console.log("SI SI SI SI")
-            if (this.selector === 2) { // COOP
+            if (this.selector === 3) { // COOP
                 this.registry.set("mode", 1);
                 this.scene.start("Game");
                 this.scene.launch("HUD"); // lanzar HUD encima del Game
             }
-            if (this.selector === 1) { // VERSUS
+            if (this.selector === 2) { // VERSUS
                 this.registry.set("mode", 2);
                 this.scene.start("Game");
                 this.scene.launch("HUD"); // lanzar HUD encima del Game
+            }
+            if (this.selector === 1) {// SCOREBOARD
+
             }
             if (this.selector === 0) { // LANGUAGE
                 if (this.language === ES) {
@@ -134,9 +196,11 @@ export class MainMenu extends Phaser.Scene {
         this.coopText.setColor("#fff");
         this.versusText.setColor("#fff");
         this.languageText.setColor("#fff");
+        this.scoreboardText.setColor("#fff");
         let text = null;
-        if (this.selector === 2) text = this.coopText;
-        if (this.selector === 1) text = this.versusText;
+        if (this.selector === 3) text = this.coopText;
+        if (this.selector === 2) text = this.versusText;
+        if (this.selector === 1) text = this.scoreboardText;
         if (this.selector === 0) text = this.languageText;
         text.setColor("#2ed12eff");
     }
