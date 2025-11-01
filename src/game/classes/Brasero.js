6b5f10bfa11fbb9201afe3105b007c3ce284c262
ErwinAndino <erwin.andino@hotmail.com>
2025-11-01 19:08:47 -0300

    mejorado el brasero para que acepte items mientras cocina y te deje sacar items mientras cocina

--- src/game/classes/Brasero.js
+++ src/game/classes/Brasero.js
@@ -13,6 +13,7 @@ export class Brasero extends KitchenBox {
         // this.holdingItem = false;
         this.coalLevel = 0;
         this.itemHolded = [];
+        this.itemCooked = [];
         this.numeroDeEtapas = 0;
         this.etapas = {};
         this.etapaActual = null;
@@ -80,16 +81,15 @@ export class Brasero extends KitchenBox {
         // Protege contra null
         if (!player.itemHolded) return false;
 
-        // Si el timer está activo, no es compatible
-        if (this.circleTimer.active) return false;
-
         // Si el aparato acepta el item
         if (this.aparatoAccepts[player.itemHolded.textureKey]) {
             console.log("%cEntro por aca", "color: green");
             this.scene.tweens.killTweensOf(player.itemHolded);
             console.log("se intento poner algo: ", player.itemHolded.dataIngredient);
 
-            this.itemHolded.unshift(player.itemHolded);
+
+
+            this.itemHolded.unshift(player.itemHolded); // coloca el carbon crudo en la segunda posicion
             this.holdingItem = true;
             this.coalLevel = Math.min(5, this.coalLevel + 1);
             this.coalFrame = Math.min(4, this.coalFrame + 1);
@@ -100,9 +100,13 @@ export class Brasero extends KitchenBox {
             this.itemHolded[0].setPosition(this.body.center.x + 1, this.body.center.y - 21);
             this.itemHolded[0].setVisible(true);
             this.itemHolded[0].setGrabbed(true);
+            console.log("itemHolded array: ", this.itemHolded.length)
+            console.log("itemCooked array: ", this.itemCooked.length)
 
             this.emitterHumo2.frequency = 500 //activa las particulas
-            this.startCook();
+            if (!this.circleTimer.active) {
+                this.startCook();
+            }
 
             return true; // compatible
         }
@@ -110,22 +114,23 @@ export class Brasero extends KitchenBox {
         return false; // no compatible
     }
     checkIfItemCanGo(player) {
-        if (this.circleTimer.active) {
-            this.circleTimer.stop()
-            if (this.actionSound) {
-                this.actionSound.stop();
-            }
-            // this.setTexture(this.textureKey)
-        }
-        player.holdingSM.changeState("ingredient", { player: player, ingredient: this.itemHolded[0] });
+        const puedeAgarrar = this.itemCooked.some(
+            item => item.dataIngredient.index === 12); //si alguno de los items del array esta cocinado continua
+        if (!puedeAgarrar) return;
+
+        player.holdingSM.changeState("ingredient", { player: player, ingredient: this.itemCooked[this.itemCooked.length - 1] }); //agarra el utlimo, que deberia estar cocinado
+        this.itemCooked.pop();
+        console.log("itemHolded array: ", this.itemHolded.length)
+        console.log("itemCooked array: ", this.itemCooked.length)
+
         this.coalLevel = Math.max(0, this.coalLevel - 1);
         this.coalFrame = Math.max(0, this.coalFrame - 1);
         this.textureCoal.setFrame(this.coalFrame);
-        this.itemHolded.shift()
         console.log(this.coalLevel)
         if (this.coalLevel === 0) {
             console.log("esta vacio ves?")
             this.itemHolded = [];
+            this.itemCooked = [];
             this.holdingItem = false;
             this.textureCoal.setVisible(false);
             this.iconoCarbon.setVisible(true);
@@ -136,6 +141,10 @@ export class Brasero extends KitchenBox {
     }
 
     startCook() {
+        const puedeCocinar = this.itemHolded.some(
+            item => item.dataIngredient.index != 12); //si alguno de los items del array no esta cocinado continua cocinando
+        if (!puedeCocinar) return;
+
         if (this.itemHolded[0] && this.itemHolded[0].dataIngredient.next && this.itemHolded[0].dataIngredient.next[this.textureKey]) {
             console.log("%cSe inicia reloj", "color: aqua")
             this.circleTimer.start()
@@ -148,10 +157,14 @@ export class Brasero extends KitchenBox {
 
 
     finishCook() {
-        this.itemHolded[0].cook(this.textureKey);
-        // this.actionFinish.play()
-        console.log("NEW COOKED ITEM: ", this.itemHolded[0].dataIngredient)
-        if (this.itemHolded[0].dataIngredient.next && this.itemHolded[0].dataIngredient.next[this.textureKey]) {
+        if (this.itemHolded.length > 0) {
+            this.itemHolded[0].cook(this.textureKey);
+            this.itemCooked.push(this.itemHolded.shift());
+            console.log("NEW COOKED ITEM: ", this.itemCooked[0].dataIngredient)
+            console.log("itemHolded array: ", this.itemHolded.length)
+            console.log("itemCooked array: ", this.itemCooked.length)
+        }
+        if (this.itemHolded[0] && this.itemHolded[0].dataIngredient.next && this.itemHolded[0].dataIngredient.next[this.textureKey]) {
             console.log("tiene que arrancar reloj")
             this.circleTimer.start()
         } else {
